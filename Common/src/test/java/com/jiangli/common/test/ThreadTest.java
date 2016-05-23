efe194d7cefb6f066fe66810bd0776c6419a0c5b
jiangli <315506754@qq.com>
2016-05-23 19:45:28 +0800

    thread

---
+++ Common/src/test/java/com/jiangli/common/test/ThreadTest.java
@@ -0,0 +1,33 @@
+package com.jiangli.common.test;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Jiangli
+ *
+ *         CreatedTime  2016/4/26 0026 17:21
+ */
+public class ThreadTest {
+    private static Logger logger = LoggerFactory.getLogger(ThreadTest.class);
+
+
+    public static void main(String[] args) {
+        System.out.println("saaa");
+        logger.debug("hahah");
+
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        });
+        thread1.start();
+//        thread1.
+
+        Thread thread2 = new Thread();
+        thread2.start();
+
+    }
+
+}
