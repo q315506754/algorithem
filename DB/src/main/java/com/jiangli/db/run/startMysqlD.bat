d34d563a9775307a1c7cdde6df9407f945ece8ca
JiangLi <jiangli@bingdian.com>
2016-07-14 17:53:32 +0800

    shell commit...

--- DB/src/main/java/com/jiangli/db/run/startMysqlD.bat
+++ DB/src/main/java/com/jiangli/db/run/startMysqlD.bat
@@ -1,6 +1,6 @@
 call "close Mysql.bat"
 set "mysql_home=C:\mysql-5.6.25-winx64"
-%mysql_home%/bin/mysqld
+%mysql_home%/bin/mysqld --init-file=%mysql_home%/my-default.ini
 ::pause
 rem comment
 %comment%
