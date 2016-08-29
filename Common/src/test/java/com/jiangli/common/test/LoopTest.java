a35789d511771a941412c7387730956ab17fcbea
jiangli <315506754@qq.com>
2016-08-29 23:31:08 +0800

    shell commit...

---
+++ Common/src/test/java/com/jiangli/common/test/LoopTest.java
@@ -0,0 +1,40 @@
+package com.jiangli.common.test;
+
+import org.junit.Test;
+
+/**
+ * Created by Jiangli on 2016/8/5.
+ */
+public class LoopTest {
+
+    /*
+    import com.sun.btrace.annotations.*;
+import static com.sun.btrace.BTraceUtils.*;
+
+@BTrace
+public class TracingScript {
+    @OnMethod(clazz="com.jiangli.common.test.LoopTest",method="add",location=@Location(Kind.RETURN))
+    public static void execute(@Return int rtn,int n) {
+        println(strcat("rs:",str(rtn)));
+    }
+}
+
+     */
+    @Test
+    public void func() {
+        int n = 0;
+        while (true) {
+            n= add(n);
+            try {
+                Thread.sleep(800);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public int add(int n) {
+        return ++n;
+
+    }
+}
