cf5b48b863bc216ef46a4cbd26260bf538d5f359
JiangLi <jiangli@bingdian.com>
2016-09-01 17:43:30 +0800

    shell commit...

---
+++ Test/src/test/java/com/jiangli/test/StringTTEst.java
@@ -0,0 +1,24 @@
+package com.jiangli.test;
+
+import org.junit.Test;
+
+/**
+ * @author Administrator
+ *
+ *         CreatedTime  2016/9/1 0001 16:38
+ */
+public class StringTTEst {
+
+    @Test
+    public void func() {
+        String s="a"+"b"+"c"+"d";
+
+        StringBuilder sb = new StringBuilder();
+        String a = "a";
+        String b = "b";
+        String c = "c";
+        String d = "d";
+        String s2 = a+b+c+d;
+    }
+
+}
