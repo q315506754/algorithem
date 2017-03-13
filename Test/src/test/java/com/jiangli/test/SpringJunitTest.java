74028a0b85b9272c636aa2a989eccd4c75dde559
jiangli <jiangli@able-elec.com>
2017-03-13 20:05:45 +0800

    shell commit...

---
+++ Test/src/test/java/com/jiangli/test/SpringJunitTest.java
@@ -0,0 +1,50 @@
+package com.jiangli.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @author Jiangli
+ *
+ *         CreatedTime  2016/4/26 0026 17:21
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath*:applicationContext.xml"})
+public class SpringJunitTest implements ApplicationContextAware {
+
+    private static Logger logger = LoggerFactory.getLogger(SpringJunitTest.class);
+    private ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+    @Test
+    public void func() {
+        logger.debug(applicationContext.toString());
+    }
+
+    @Test
+    public void func2() {
+        logger.debug(applicationContext.toString());
+    }
+
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"classpath*:applicationContext.xml"});
+        String[] beanDefinitionNames = context.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            System.out.println(beanDefinitionName);
+        }
+
+    }
+
+}
