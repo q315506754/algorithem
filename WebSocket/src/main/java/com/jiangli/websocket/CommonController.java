f847dd742902071edf0e7230f5ca03a8f5313ae8
jiangli <jiangli@able-elec.com>
2017-05-12 17:32:21 +0800

    shell commit...

---
+++ WebSocket/src/main/java/com/jiangli/websocket/CommonController.java
@@ -0,0 +1,35 @@
+package com.jiangli.websocket;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpSession;
+
+/**
+ * @author Jiangli
+ *
+ *         CreatedTime  2015/6/9 0009 10:39
+ */
+@Controller
+@RequestMapping(value = "/")
+public class CommonController {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+
+    @Autowired
+    private HttpSession session;
+
+    @Autowired
+    private ServletContext servletContext;
+
+
+    public CommonController() {
+        logger.debug("CommonController construct...");
+    }
+
+
+}
---
+++ WebSocket/src/main/webapp/index.jsp
@@ -0,0 +1,19 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Administrator
+  Date: 2016/7/29 0029
+  Time: 16:31
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <script>
+      
+  </script>
+  <body>
+  $END$ sdsdsd
+  </body>
+</html>
