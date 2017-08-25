478759c1ce40d5d247d7c6eeff723d2e877ca698
jiangli <jiangli@able-elec.com>
2017-08-25 17:36:30 +0800

    shell commit...

--- Doc/src/main/java/com/jiangli/doc/txt/RedisBuilder.kt
+++ Doc/src/main/java/com/jiangli/doc/txt/RedisBuilder.kt
@@ -9,11 +9,13 @@ package com.jiangli.doc.txt
 
 
 fun main(args: Array<String>) {
-//    val pool = Redis.getYanfaPool()
-//    val jdbc = DB.getJDBCForYanFa()
+//    println(System.currentTimeMillis())
+//    return
+    val pool = Redis.getYanfaPool()
+    val jdbc = DB.getJDBCForYanFa()
 
-    val pool = Redis.getYufaPool()
-    val jdbc = DB.getJDBCForYuFa()
+//    val pool = Redis.getYufaPool()
+//    val jdbc = DB.getJDBCForYuFa()
 
     //名师id列表
     val idList = DB.getSrc1IdList(jdbc)
@@ -23,9 +25,9 @@ fun main(args: Array<String>) {
         val jedis = it
         val smembers = jedis.smembers("th:lock:totals")
 
-//        smembers.forEach {
-//            jedis.executeDel(it)
-//        }
+        smembers.forEach {
+            jedis.executeDel(it)
+        }
 
 //        println(smembers)
 
@@ -48,9 +50,10 @@ fun main(args: Array<String>) {
 //            jedis.executeDel("th:byConcernIds:${it}") //被关注人的teacherId 缓存300个
             jedis.executeDel("th:byConcernNum:${it}") //被关注数量
 
-//            jedis.executeDel("th:personalGloryIds:${it}") //个人荣耀 列表
-//            jedis.executeDel("th:wonderVideo:ids:${it}") //风采视频 列表
-//            jedis.executeDel("th:quotations:ids:${it}") //我的语录 列表
+            jedis.executeDel("th:personalGloryIds:${it}") //个人荣耀 列表
+            jedis.executeDel("th:wonderVideo:ids:${it}") //风采视频 列表
+            jedis.executeDel("th:quotations:ids:${it}") //我的语录 列表
+//            jedis.zrev
 //
 //            jedis.executeDel("th:filtrationMeetCourseIds:${it}") //被过滤的见面课ids
 //            jedis.executeDel("th:filtrationCourse:ids:${it}") //被过滤的课程ids
