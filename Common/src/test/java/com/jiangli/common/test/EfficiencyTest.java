6ec248d9445941fca69d947d308d1ddc416d8e9f
jiangli <jiangli@able-elec.com>
2017-09-25 19:04:49 +0800

    shell commit...

---
+++ Common/src/test/java/com/jiangli/common/test/EfficiencyTest.java
@@ -0,0 +1,39 @@
+package com.jiangli.common.test;
+
+class B {
+
+}
+
+public class EfficiencyTest {
+
+    public static long timeDiff(long old) {
+        return System.currentTimeMillis() - old;
+    }
+
+    public static void main(String args[]) throws Exception {
+
+        long numTrials = (long) Math.pow(10, 8);
+
+        long millis;
+
+        millis = System.currentTimeMillis();
+
+        for (int i=0; i<numTrials; i++) {
+            new B();
+        }
+        System.out.println("Normal instaniation took: "
+                 + timeDiff(millis) + "ms");
+
+        millis = System.currentTimeMillis();
+
+        Class<B> c = B.class;
+
+        for (int i=0; i<numTrials; i++) {
+            c.newInstance();
+        }
+
+        System.out.println("Reflecting instantiation took:" 
+              + timeDiff(millis) + "ms");
+
+    }
+}
