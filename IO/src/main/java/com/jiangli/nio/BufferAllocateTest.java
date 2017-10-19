5876e4332a0ef8c91ee6a68578b604043caa49fb
jiangli <jiangli@able-elec.com>
2017-10-19 17:41:11 +0800

    shell commit...

--- IO/src/main/java/com/jiangli/nio/BufferAllocateTest.java
+++ IO/src/main/java/com/jiangli/nio/BufferAllocateTest.java
@@ -1,9 +1,6 @@
 package com.jiangli.nio;
 
-import org.apache.commons.beanutils.BeanUtils;
-
-import java.lang.reflect.InvocationTargetException;
-import java.nio.Buffer;
+import java.lang.reflect.Field;
 import java.nio.ByteBuffer;
 
 /**
@@ -11,40 +8,43 @@ import java.nio.ByteBuffer;
  *
  *         CreatedTime  2016/6/23 0023 15:36
  */
-public class BufferTest {
+public class BufferAllocateTest {
     static int i = 1;
-    public static void main(String[] args) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
-        ByteBuffer allocate = ByteBuffer.allocate(1024);
+    public static void main(String[] args) throws Exception {
+        ByteBuffer buffer=ByteBuffer.allocateDirect(0);
+        Class<?> c = Class.forName("java.nio.Bits");
+        Field maxMemory = c.getDeclaredField("maxMemory");
+        maxMemory.setAccessible(true);
+        synchronized (c) {
+            Long maxMemoryValue = (Long)maxMemory.get(null);
+            System.out.println("maxMemoryValue:"+maxMemoryValue);
+        }
+        System.out.println("maxMemoryValue:"+sun.misc.VM.maxDirectMemory());
+
+//        一种是heap ByteBuffer,该类对象分配在JVM的堆内存里面，直接由Java虚拟机负责垃圾回收，
+//        JVM堆内存大小可以通过-Xmx来设置
+//        ByteBuffer allocate = ByteBuffer.allocate(1234567891);
+
+
+//        一种是direct ByteBuffer是通过jni在虚拟机外内存中分配的 通过jmap无法查看该快内存的使用情况。只能通过top来看它的内存使用情况。
+//        通过-XX:MaxDirectMemorySize来设置，此参数的含义是当Direct ByteBuffer分配的堆外内存到达指定大小后，即触发Full GC。
+
+
+        ByteBuffer allocate = ByteBuffer.allocateDirect(1234567891);
 //        ByteBuffer allocate = ByteBuffer.allocate(1);
-        System.out.println(BeanUtils.describe(allocate));
+//        System.out.println(BeanUtils.describe(allocate));
+        System.out.println(allocate);
 
-        print(allocate);
-        allocate.put((byte)12);
-        allocate.put((byte)12);
-        allocate.put((byte)12);
-        print(allocate);
 
-        allocate.mark();
-        allocate.put((byte) 23);
-        allocate.put((byte) 34);
-        allocate.put((byte)45);
-        print(allocate);
+//        direct ByteBuffer通过full gc来回收内存的，direct ByteBuffer会自己检测情况而调用system.gc()，
+// 但是如果参数中使用了DisableExplicitGC那么就无法回收该快内存了，
+// -XX:+DisableExplicitGC标志自动将System.gc()调用转换成一个空操作，
+// 就是应用中调用System.gc()会变成一个空操作。那么如果设置了就需要我们手动来回收内存了
 
-        allocate.reset();
-        print(allocate);
 
-        Buffer flip = allocate.flip();
-        print(allocate);
-
-        System.out.println("sss:"+new String(((ByteBuffer)flip).array(),0,flip.limit())+":eee");
-
+//        1:多用于网络编程中，实现zero copy,数据不需要再native memory和jvm memory中来回copy
+//        2:由于构造和析构Direct Buffer时间成本高，建议使用缓冲池，参见netty的实现
     }
 
-    public static void print(ByteBuffer allocate) {
-        System.out.println("--------"+(i++)+"---------");
-        System.out.println("position:"+allocate.position());
-        System.out.println("limit:"+allocate.limit());
-        System.out.println("capacity:"+allocate.capacity());
-    }
 
 }
--- IO/src/main/java/com/jiangli/nio/VmTest.java
+++ IO/src/main/java/com/jiangli/nio/VmTest.java
@@ -1,9 +1,21 @@
 package com.jiangli.nio;
 
+import sun.misc.VM;
+
 /**
  * @author Jiangli
  * @date 2017/10/19 10:23
  */
 public class VmTest {
+    public static void main(String[] args) {
+        //没有配置MaxDirectMemorySize的，因此MaxDirectMemorySize的大小即等于-Xmx  默认1897922560 约1.767578125G
+
+        //当配置了-Xmx1280k 则打印1572864=1536k  1.2倍
+
+//        -XX:MaxDirectMemorySize=111k  指定后打印 113664=111k
+        System.out.println(VM.maxDirectMemory());
+
+        //避免-Xmx + Direct Memory超出物理内存大小的现象。
+    }
 
 }
