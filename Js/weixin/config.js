71cd2ef84f6c0eb4b0202f9329b5d6433dac91a8
jiangli <jiangli@able-elec.com>
2017-11-09 19:30:46 +0800

    shell commit...

--- Js/weixin/config.js
+++ Js/weixin/config.js
@@ -1,5 +1,6 @@
 var config = {
     weixinhao:"gh_8a9b3859d30c",
     appID:"wx4c5ab06eb32fb003",
-    appsecret:"1f43034386edaaca2b03e78db01d6002"
+    appsecret:"1f43034386edaaca2b03e78db01d6002",
+    merchantId:"1481522292",
 }
