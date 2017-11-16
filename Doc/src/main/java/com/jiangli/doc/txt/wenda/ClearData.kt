100155c2763eb74fe79e6867dff69bc0b9f37614
jiangli <jiangli@able-elec.com>
2017-11-16 21:31:48 +0800

    shell commit...

--- Doc/src/main/java/com/jiangli/doc/txt/wenda/ClearData.kt
+++ Doc/src/main/java/com/jiangli/doc/txt/wenda/ClearData.kt
@@ -1,8 +1,6 @@
 package com.jiangli.doc.txt.wenda
 
 import com.jiangli.doc.txt.DB
-import com.jiangli.doc.txt.Redis
-import com.jiangli.doc.txt.execute
 import com.jiangli.doc.txt.importdata.delKeysPage
 import com.jiangli.doc.txt.importdata.queryDistinct
 import org.springframework.jdbc.core.JdbcTemplate
@@ -17,17 +15,17 @@ import org.springframework.jdbc.core.JdbcTemplate
 
 fun main(args: Array<String>) {
 
-    val pool = Redis.getYanfaPool()
+//    val pool = Redis.getYanfaPool()
 //    val pool = Redis.getYufaPool()
-    val jdbc = DB.getJDBCForYanFa()
-//    val jdbc = DB.getWendaJDBCForWaiWang()
+//    val jdbc = DB.getJDBCForYanFa()
+    val jdbc = DB.getWendaJDBCForWaiWang()
 //    val query = jdbc.query("select distinct CREATE_USER from ZHS_BBS.QA_QUESTION", ColumnMapRowMapper())
 
 
-// update QA_QUESTION set IS_DELETED = 1 where IS_DELETED=0;
-// update QA_ANSWER set IS_DELETED = 1 where IS_DELETED=0;
-// update QA_COMMENT set IS_DELETED = 1 where IS_DELETED=0;
-// update QA_OPERATION set IS_DELETED = 1 where IS_DELETED=0 AND OPERATION_TYPE=2;
+// update ZHS_BBS.QA_QUESTION set IS_DELETED = 1 where IS_DELETED=0;
+// update ZHS_BBS.QA_ANSWER set IS_DELETED = 1 where IS_DELETED=0;
+// update ZHS_BBS.QA_COMMENT set IS_DELETED = 1 where IS_DELETED=0;
+// update ZHS_BBS.QA_OPERATION set IS_DELETED = 1 where IS_DELETED=0 AND OPERATION_TYPE=2;
 
     val ANCESTOR_COURSE_ID = queryDistinct(jdbc, " ZHS_BBS.QA_QUESTION", "ANCESTOR_COURSE_ID")
     println(ANCESTOR_COURSE_ID)
@@ -39,6 +37,8 @@ fun main(args: Array<String>) {
     val keys = arrayListOf<String>()
 
 
+//    qa:ancestorsCourse:questionIds:2017824 0 -1
+//    qa:ancestorsCourse:hot:questionIds:2017824 0 -1
     ANCESTOR_COURSE_ID.forEach {
         it?.let {
             keys.add("qa:ancestorsCourse:questionIds:$it")   //祖先回答时间倒序
@@ -50,7 +50,7 @@ fun main(args: Array<String>) {
         it?.let {
             keys.add("qa:my:questionIds:$it")   //我的提问
             keys.add("qa:answer:myAnser:$it")  //我的回答
-            keys.add("qa:operation:myQuestioninfo:$it:2")  //我的围观
+            keys.add("qa:operation:mylookerquestion:$it")  //我的围观
             keys.add("qa:comment:myComment:$it")  //我的评论
 
 //            keys.add("qa:$it")  //
@@ -60,12 +60,12 @@ fun main(args: Array<String>) {
     delKeysPage(keys)
 
 
-    pool.execute {
-        val redis = it
-        keys.forEach {
-            redis.del(it)
-        }
-    }
+//    pool.execute {
+//        val redis = it
+//        keys.forEach {
+//            redis.del(it)
+//        }
+//    }
 }
 
 
